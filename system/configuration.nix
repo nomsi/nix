ba371de0e5522ed1d5e75721e4bf5537da89da88
Emi Jade <me@emi.lgbt>
2025-03-17 17:00:14 -0700

    Testing removing docker

--- system/configuration.nix
+++ system/configuration.nix
@@ -100,7 +100,6 @@
   
   # Docker
   virtualisation.docker.enable = true;
-  virtualisation.docker.storageDriver = "btrfs";
   virtualisation.docker.rootless = {
     enable = true;
     setSocketVariable = true;
